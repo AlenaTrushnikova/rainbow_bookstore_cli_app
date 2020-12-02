6459e5f944cbc0ce5d7857100411807bc9a45c9a
AlenaTrushnikova <alena.yu.t@gmail.com>
2020-12-02 14:50:49 -0800

    Find Book By and Purchase Methods Added

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,13 @@
+require 'pry'
 class Book < ActiveRecord::Base
     has_many :purchases
     has_many :shoppers, through: :purchases
+
+    def self.find_by_category(cat)
+        Book.where(category: cat).map{|book| book.title}
+    end
+
+    def self.find_by_author(author)
+        Book.where(author: author).map{|book| book.title}
+    end
 end
--- app/models/shopper.rb
+++ app/models/shopper.rb
@@ -4,9 +4,9 @@ class Shopper < ActiveRecord::Base
     has_many :books, through: :purchases
 
     def buy(book_title)
-        book_id = Book.find_by(title: book_title).id
-binding.pry        
-        Purchase.create()
-
+        book_id = Book.find_by(title: book_title).id      
+        Purchase.create(book_id: book_id, shopper_id: self.id)
     end
+
+    
 end
