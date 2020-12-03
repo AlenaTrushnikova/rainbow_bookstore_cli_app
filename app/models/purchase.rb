934ff6140717bb4a80a182b7c23473eec7028fbf
github username <github email address>
2020-12-03 15:02:07 -0800

    return method

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,5 +1,5 @@
 class Purchase < ActiveRecord::Base
     belongs_to :book
     belongs_to :shopper
-     
+
 end
--- app/program/bookstore.rb
+++ app/program/bookstore.rb
@@ -2,7 +2,7 @@ require_all 'app'
 require 'pry'
 
 class Visit
-    attr_reader :name, :age
+    attr_reader :name, :age, :me
     def initialize(name, age)
         @name = name
         @age = age
@@ -16,39 +16,55 @@ class Visit
     end
 
     def options
-        puts "1. Purchase a book\n2. Return a book\n3. Browse by Category\n4. Browse by Author\n5. See top sellers"
-        print "Please enter a number, or type 'exit'."
-        option = gets.chomp
-        while option != 'exit' do
-            case option.downcase
+        puts "Main Menu\n1. Purchase a book\n2. Return a book\n3. Browse by Category\n4. Browse by Author\n5. See top sellers\n6. exit"
+        print "Please enter a number, or type 'exit'. "
+        input = STDIN.gets.chomp
+        while input != 'exit' && input != "6" do
+            case input.downcase
             when "1"
                 self.purchase
             when "2"
+                self.return
             when "3"
             when "4"
             when "5"
             else
                 print "Invalid input, please try again."
-                option = gets.chomp
+                input = STDIN.gets.chomp
             end
         end
     end
 
     def purchase
         print "What is the title of the book you'd like to buy? "
-        response = gets.chomp
-        while response != 'return'
-            if Book.find_by(title: response) == nil
-                puts "Sorry, we don't have this book in our store. Try another title or type 'return'."
-                response = gets.chomp
+        input = STDIN.gets.chomp
+        while input != 'return' do
+            if Book.find_by(title: input) == nil
+                print "Sorry, we don't have this book in our store. Try another title or type 'return' to go to the main menu. "
+                input = STDIN.gets.chomp
             else
-                Shopper.find(@me.id).buy(response)
-                puts "Thank you for buying #{response}, now fork over $#{Book.find_by(title: response).price}."
+                Shopper.find(@me.id).buy(input)
+                puts "Thank you for buying #{input}, now fork over $#{Book.find_by(title: input).price}."
                 print "What would you like to do next?"
                 self.options
             end
         end
-        puts "Choose a new option."
         self.options
     end
+
+    def return
+        print "What is the title of the book you'd like to return? "
+        input = STDIN.gets.chomp
+        while input != 'return' do
+            if old_purchase = @me.purchases.select{|pur| pur.book.title == input}[0]
+                old_purchase.update(return?:true)
+                puts "Thank you for returning #{input}"
+                self.options
+            else
+                puts "Sorry, you cannot return this book, you didn't buy it at this store."
+                self.options
+            end
+        end
+    end
+    
 end
